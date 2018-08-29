ab1047afdca16acd4c72fe3b8fcdab7da5ab2b73
ductoanthanh <toanth@metropolia.fi>
2018-08-30 00:49:45 +0300

    Refactor Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 // if a component just receive data from props and return JSX => stateless functional component
 const Header = (props) => (
         <header className="top">
-                <h1>Catch 
+                <h1>Fish 
                     <span className="ofThe">
                     <span className="of">Of</span> 
                     <span className="the">The</span>
@@ -19,23 +19,5 @@ const Header = (props) => (
 Header.propTypes = {
     tagline: PropTypes.string.isRequired
 };
-// class Header extends React.Component {
-//     render() {
-//         return (
-//             <header className="top">
-//                 <h1>Catch 
-//                     <span className="ofThe">
-//                     <span className="of">Of</span> 
-//                     <span className="the">The</span>
-//                     </span> 
-//                     Day</h1>
-//                 <h3 className="tagline">
-//                     <span>{this.props.tagline}</span>
-//                 </h3>
-//             </header>
-            
-//         );
-//     }
-// }
 
 export default Header;
